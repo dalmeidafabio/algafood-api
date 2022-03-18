c76dd5b9cc57932526c7c624383857aaf68742ee
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-03-18 09:54:59 -0300

    Initial commit

---
+++ src/main/java/com/algaworks/algafood/listener/EmissaoNotaFiscalService.java
@@ -0,0 +1,15 @@
+package com.algaworks.algafood.listener;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.di.service.ClienteAtivadoEvent;
+
+@Component
+public class EmissaoNotaFiscalService {
+
+	@EventListener
+	public void clienteAtivadoListener(ClienteAtivadoEvent event) {
+		System.out.println("Emitindo NF para cliente " + event.getCliente().getNome());
+	}
+}
---
+++ src/main/java/com/algaworks/algafood/listener/NotificacaoService.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood.listener;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.di.notificacao.NivelUrgencia;
+import com.algaworks.algafood.di.notificacao.Notificador;
+import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
+import com.algaworks.algafood.di.service.ClienteAtivadoEvent;
+
+@Component
+public class NotificacaoService {
+	
+	@Autowired
+	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
+	private Notificador notificador;
+		
+	@EventListener
+	public void clienteAtivadoListener(ClienteAtivadoEvent event) {
+		notificador.notificar(event.getCliente(), "Seu cadastro no sistema está ativo.");
+	}
+}
