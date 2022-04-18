94d156d9f85a4c7b07e01f4a61001e2f9ff8334c
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-04-18 17:47:44 -0300

    Aula 8.27
    Aula 8.27

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -165,6 +165,28 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
 	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> handleUncaught(Exception ex, WebRequest request){
+		
+		HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+		ProblemType problemType = ProblemType.ERRO_DE_SISTEMA;
+		
+	    String detail = "Ocorreu um erro interno inesperado no sistema. "
+	            + "Tente novamente e se o problema persistir, entre em contato "
+	            + "com o administrador do sistema.";		
+		
+	    // Importante colocar o printStackTrace (pelo menos por enquanto, que não estamos
+	    // fazendo logging) para mostrar a stacktrace no console
+	    // Se não fizer isso, você não vai ver a stacktrace de exceptions que seriam importantes
+	    // para você durante, especialmente na fase de desenvolvimento
+	    ex.printStackTrace();
+	    
+		Problem problem = createProblemBuilder(status, problemType, detail)
+				.build();
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);		
+	}
+	
 	@Override
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
