656dca61166c026126f5e16fe0d684774453bed2
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-04-20 09:26:59 -0300

    Aula 9.14
    9.14. Usando o Resource Bundle do Spring como Resource Bundle do Bean Validation

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -26,7 +26,7 @@ import javax.validation.groups.Default;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import com.algaworks.algafood.Groups;
+import com.algaworks.algafood.core.validation.Groups;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
@@ -46,6 +46,7 @@ public class Restaurante {
 	@Column(nullable = false)
 	private String nome;
 	
+	@NotNull
 	@PositiveOrZero
 	@Column(nullable = false)
 	private BigDecimal taxaFrete;
