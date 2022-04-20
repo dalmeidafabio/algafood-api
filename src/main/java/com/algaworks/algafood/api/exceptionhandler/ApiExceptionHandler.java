09283120b388682ca393815409d6977e0f54946e
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-04-19 21:23:53 -0300

    Aula 9.11
    9.11. Customizando e resolvendo mensagens de validação globais em Resource Bundle

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -6,6 +6,9 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,6 +36,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 			"Ocorreu um erro interno inesperado no sistema. "
 		            + "Tente novamente e se o problema persistir, entre em contato "
 		            + "com o administrador do sistema.";
+	
+	@Autowired
+	private MessageSource messageSource;
 
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
@@ -45,7 +51,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		} else if (rootCause instanceof PropertyBindingException) {
 			return handlePropertyBinding((PropertyBindingException) rootCause, headers, status, request);
 		}
-		
 		
 		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
 		String detail = "O corpo da requisição está inválido. Verifique o erro de sintaxe.";
@@ -67,12 +72,17 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	    BindingResult bindingResult = ex.getBindingResult();
 	    
 	    List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
-	    		.map(fieldError -> Problem.Field.builder()
-	    				.name(fieldError.getField())
-	    				.userMessage(fieldError.getDefaultMessage())
-	    				.build())
-	    		.collect(Collectors.toList());
-	    
+	    		.map(fieldError -> {
+	    			String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+	    			
+		    		return Problem.Field.builder()
+		    				.name(fieldError.getField())
+		    				.userMessage(message)
+		    				.build();
+		    		
+	    		})
+				.collect(Collectors.toList());
+				
 		Problem problem = createProblemBuilder(status, problemType, detail)
 				.userMessage(detail)
 				.fields(problemFields)
