3a38bf5ef714a63c3bc2c0a2ea10884f1b514b20
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-04-27 15:39:08 -0300

    Aula 10.8
    10.8. Implementando Testes de API com REST Assured e validando o código de status HTTP

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,76 +1,36 @@
 package com.algaworks.algafood;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static io.restassured.RestAssured.given;
 
-import javax.validation.ConstraintViolationException;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
-import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
-import com.algaworks.algafood.domain.model.Cozinha;
-import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
-class CadastroCozinhaIT {
-	
-	@Autowired
-	CadastroCozinhaService cadastroCozinha;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CadastroCozinhaIT {
 
+	@LocalServerPort
+	private int port;
+	
 	@Test
-	public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
-		// cenário
-		Cozinha novaCozinha = new Cozinha();
-		novaCozinha.setNome("Chinesa");
+	public void deveRetornarStatus200QuandoConsultarCozinhas() {
 		
-		// ação
-		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		
-		// validação
-		assertThat(novaCozinha).isNotNull();
-		assertThat(novaCozinha.getId()).isNotNull();
-	}	
-	
-	@Test
-	public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
-	   Cozinha novaCozinha = new Cozinha();
-	   novaCozinha.setNome(null);
-	   
-	   ConstraintViolationException erroEsperado =
-	      Assertions.assertThrows(ConstraintViolationException.class, () -> {
-	         cadastroCozinha.salvar(novaCozinha);
-	      });
-	   
-	   assertThat(erroEsperado).isNotNull();
-	}
-	
-	@Test
-	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
-		Long cozinhaId = 1L;   
-		
-		EntidadeEmUsoException erroEsperado =
-			      Assertions.assertThrows(EntidadeEmUsoException.class, () -> {
-			    	  cadastroCozinha.excluir(cozinhaId);
-			      });
-	   
-		assertThat(erroEsperado).isNotNull();
-	}
-	
-	@Test
-	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
-		Long cozinhaId = Long.MAX_VALUE;   
-	
-		CozinhaNaoEncontradaException erroEsperado =
-			      Assertions.assertThrows(CozinhaNaoEncontradaException.class, () -> {
-			    	  cadastroCozinha.excluir(cozinhaId);
-			      });
-	   
-		assertThat(erroEsperado).isNotNull();
+		given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
 	}
 }
---
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaTesteIntegracaoIT.java
@@ -0,0 +1,77 @@
+package com.algaworks.algafood;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+
+///ESSA CLASSE FOI REMOVIDA DO PROJETO DO CURSO - DEIXEI APENAS PARA CONSULTA
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class CadastroCozinhaTesteIntegracaoIT {
+	
+	@Autowired
+	CadastroCozinhaService cadastroCozinha;
+
+	@Test
+	public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
+		// cenário
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");
+		
+		// ação
+		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+		
+		// validação
+		assertThat(novaCozinha).isNotNull();
+		assertThat(novaCozinha.getId()).isNotNull();
+	}	
+	
+	@Test
+	public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
+	   Cozinha novaCozinha = new Cozinha();
+	   novaCozinha.setNome(null);
+	   
+	   ConstraintViolationException erroEsperado =
+	      Assertions.assertThrows(ConstraintViolationException.class, () -> {
+	         cadastroCozinha.salvar(novaCozinha);
+	      });
+	   
+	   assertThat(erroEsperado).isNotNull();
+	}
+	
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+		Long cozinhaId = 1L;   
+		
+		EntidadeEmUsoException erroEsperado =
+			      Assertions.assertThrows(EntidadeEmUsoException.class, () -> {
+			    	  cadastroCozinha.excluir(cozinhaId);
+			      });
+	   
+		assertThat(erroEsperado).isNotNull();
+	}
+	
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+		Long cozinhaId = Long.MAX_VALUE;   
+	
+		CozinhaNaoEncontradaException erroEsperado =
+			      Assertions.assertThrows(CozinhaNaoEncontradaException.class, () -> {
+			    	  cadastroCozinha.excluir(cozinhaId);
+			      });
+	   
+		assertThat(erroEsperado).isNotNull();
+	}
+}
