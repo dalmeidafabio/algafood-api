99ad12094f94256ed191e37f9ff23cbaa4138040
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-04-28 13:36:34 -0300

    Aula 10.11
    10.11. Entendendo o problema da ordem de execução dos testes

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -48,5 +48,17 @@ public class CadastroCozinhaIT {
 		.then()
 			.body("", hasSize(4))
 			.body("nome", hasItems("Indiana", "Tailandesa"));
-	}	
+	}
+	
+	@Test
+	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+		given()
+			.body("{ \"nome\": \"Chinesa\" }")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
+	}
 }
