84d165e302779e6bb4ef4b70a49bbc2249737038
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-05-02 16:09:19 -0300

    Aula 11.15
    11.15. Entendendo a estratégia padrão do ModelMapper para correspondência de propriedades

--- src/main/java/com/algaworks/algafood/api/model/dto/RestauranteModel.java
+++ src/main/java/com/algaworks/algafood/api/model/dto/RestauranteModel.java
@@ -11,6 +11,9 @@ public class RestauranteModel {
 
 	private Long id;
 	private String nome;
-	private BigDecimal taxaFrete;
+	private BigDecimal frete;
 	private CozinhaModel cozinha;
+	
+	private String nomeCozinha;
+	private Long idCozinha;
 }
