521353cb3035da951b25be5e0de20944be8506e8
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-05-02 11:00:14 -0300

    Aula 11.7
    11.7. Configurando e refatorando o projeto para usar UTC

--- src/main/java/com/algaworks/algafood/domain/model/mixin/RestauranteMixin.java
+++ src/main/java/com/algaworks/algafood/domain/model/mixin/RestauranteMixin.java
@@ -1,6 +1,6 @@
 package com.algaworks.algafood.domain.model.mixin;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 import javax.persistence.Embedded;
@@ -21,10 +21,10 @@ public abstract class RestauranteMixin {
 	private Endereco endereco;
 	
 	@JsonIgnore
-	private LocalDateTime dataCadastro;
+	private OffsetDateTime dataCadastro;
 	
 	@JsonIgnore
-	private LocalDateTime dataAtualizacao;
+	private OffsetDateTime dataAtualizacao;
 	
 	@JsonIgnore
 	private List<FormaPagamento> formasPagamento;
