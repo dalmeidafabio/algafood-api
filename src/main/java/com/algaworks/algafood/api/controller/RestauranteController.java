5cb57784089329e80a6330eca76fb4e5af3d6b70
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-05-09 10:58:26 -0300

    Aula 12.7
    12.7. Refatorando serviço de cadastro de restaurante para incluir endereço

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -20,6 +20,7 @@ import com.algaworks.algafood.api.assembler.RestauranteInputDisassembler;
 import com.algaworks.algafood.api.assembler.RestauranteModelAssembler;
 import com.algaworks.algafood.api.model.dto.RestauranteModel;
 import com.algaworks.algafood.api.model.dto.input.RestauranteInput;
+import com.algaworks.algafood.domain.exception.CidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Restaurante;
@@ -60,9 +61,9 @@ public class RestauranteController {
 			Restaurante restaurante = restauranteInputDisassembler.toDomainObject(restauranteInput);
 			
 			return restauranteModelAssembler.toModel(cadastroRestaurante.salvar(restaurante));
-		} catch (CozinhaNaoEncontradaException e) {
+		} catch (CozinhaNaoEncontradaException | CidadeNaoEncontradaException e) {
 			throw new NegocioException(e.getMessage());
-		}			
+		} 			
 	}
 	
 	@PutMapping("{restauranteId}")
@@ -75,7 +76,7 @@ public class RestauranteController {
 				restauranteInputDisassembler.copyToDomainObject(restauranteInput, restauranteAtual);
 
 				return restauranteModelAssembler.toModel(cadastroRestaurante.salvar(restauranteAtual));
-			} catch (CozinhaNaoEncontradaException e) {
+			} catch (CozinhaNaoEncontradaException | CidadeNaoEncontradaException e) {
 				throw new NegocioException(e.getMessage());
 			}	
 	}
