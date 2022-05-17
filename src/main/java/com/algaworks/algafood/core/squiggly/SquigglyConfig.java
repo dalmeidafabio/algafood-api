d3f56d784d3ca2a9fd4b85db62184fcbfa5a1bd2
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-05-16 22:59:57 -0300

    Aula 13.3
    13.3. Limitando os campos retornados pela API com Squiggly

---
+++ src/main/java/com/algaworks/algafood/core/squiggly/SquigglyConfig.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.core.squiggly;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.bohnman.squiggly.Squiggly;
+import com.github.bohnman.squiggly.web.RequestSquigglyContextProvider;
+import com.github.bohnman.squiggly.web.SquigglyRequestFilter;
+
+@Configuration
+public class SquigglyConfig {
+
+	// ESSA BIBLIOTECA NÃO TEM ATUALIZAÇÃO DESDE 2019. NÃO FUNCIONOU NO MEU PROJETO!
+	
+	@Bean
+	public FilterRegistrationBean<SquigglyRequestFilter> squigglyRequestFilter(ObjectMapper objectMapper){
+		Squiggly.init(objectMapper, new RequestSquigglyContextProvider());
+		
+		var filterRegistrarion = new FilterRegistrationBean<SquigglyRequestFilter>();
+		
+		filterRegistrarion.setFilter(new SquigglyRequestFilter());
+		filterRegistrarion.setOrder(1);
+		
+		return filterRegistrarion;
+	}
+}
