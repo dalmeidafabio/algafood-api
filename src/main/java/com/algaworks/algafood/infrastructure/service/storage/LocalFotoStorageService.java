b70ebd8458c5afb44451c494bb6e5b173255e7d0
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-05-24 15:28:37 -0300

    Aula 14.10
    14.10. Implementando a remoção e substituição de arquivos de fotos no serviço de armazenagem

--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
@@ -32,4 +32,14 @@ public class LocalFotoStorageService implements FotoStorageService {
 		return diretorioFotos.resolve(Path.of(nomeArquivo));
 	}
 
+	@Override
+	public void remover(String nomeArquivo) {
+		try {
+			Path arquivoPath = getArquivoPath(nomeArquivo);
+			Files.deleteIfExists(arquivoPath);
+		} catch (Exception e) {
+			throw new StorageException("Não foi possível excluir arquivo.", e);
+		}
+	}
+
 }
