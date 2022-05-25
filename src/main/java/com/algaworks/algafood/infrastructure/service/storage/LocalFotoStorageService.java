870f0f70955d1d2c46391ffe84923fbcbbab2a5d
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-05-25 11:54:41 -0300

    Aula 14.20
    14.20. Criando bean de propriedades de configuração dos serviços de storage

--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
@@ -4,17 +4,18 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
+import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.service.FotoStorageService;
 
-@Service
+//@Service
 public class LocalFotoStorageService implements FotoStorageService {
 
-	@Value("${algafood.storage.local.diretorio-fotos}")
-	private Path diretorioFotos;
+	@Autowired
+	private StorageProperties storageProperties;
 	
 	@Override
 	public void armazenar(NovaFoto novaFoto) {
@@ -30,7 +31,7 @@ public class LocalFotoStorageService implements FotoStorageService {
 	}
 	
 	private Path getArquivoPath(String nomeArquivo) {
-		return diretorioFotos.resolve(Path.of(nomeArquivo));
+		return storageProperties.getLocal().getDiretorioFotos().resolve(Path.of(nomeArquivo));
 	}
 
 	@Override
@@ -46,7 +47,7 @@ public class LocalFotoStorageService implements FotoStorageService {
 	@Override
 	public InputStream recuperar(String nomeArquivo) {
 		try {
-			Path arquivoPath = diretorioFotos.resolve(Path.of(nomeArquivo));
+			Path arquivoPath = storageProperties.getLocal().getDiretorioFotos().resolve(Path.of(nomeArquivo));
 			
 			return Files.newInputStream(arquivoPath);
 		} catch (Exception e) {
