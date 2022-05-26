b9d24258a048033867265b20b36b622273d8d0f9
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-05-26 17:24:16 -0300

    Aula 14.26
    14.26. Selecionando a implementação do serviço de storage de fotos

--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
@@ -4,7 +4,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
 import com.algaworks.algafood.core.storage.StorageProperties;
