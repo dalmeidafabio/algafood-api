99c13ceaffe0f463c9e64b22bb1185baec491065
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-05-26 10:50:59 -0300

    Aula 14.23
    14.23. Implementando a inclusão de objetos no bucket da Amazon S3

--- src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -46,6 +46,7 @@ public class CatalogoFotoProdutoService {
 		
 		NovaFoto novaFoto = NovaFoto.builder()
 				.nomeArquivo(foto.getNomeArquivo())
+				.contentType(foto.getContentType())
 				.inputStream(dadosArquivo)
 				.build();
 		
