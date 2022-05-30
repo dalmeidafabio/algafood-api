8f927908a8031d0f899afaa78190f42c9ecc9b9d
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-05-30 14:03:47 -0300

    Aula 15.4
    15.4. Usando o serviço de envio de e-mails na confirmação de pedidos

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -82,14 +82,13 @@ insert into grupo (id, nome) values (1, 'Gerente'), (2, 'Vendedor'), (3, 'Secret
 insert into grupo_permissao (grupo_id, permissao_id) values (1, 1), (1, 2), (2, 1), (2, 2), (3, 1); 
 
 insert into usuario (id, nome, email, senha, data_cadastro) values
-(1, 'João da Silva', 'joao.ger@algafood.com', '123', utc_timestamp),
-(2, 'Maria Joaquina', 'maria.vnd@algafood.com', '123', utc_timestamp),
-(3, 'José Souza', 'jose.aux@algafood.com', '123', utc_timestamp),
-(4, 'Sebastião Martins', 'sebastiao.cad@algafood.com', '123', utc_timestamp),
-insert into usuario (id, nome, email, senha, data_cadastro) values
-(5, 'Manoel Lima', 'manoel.loja@gmail.com', '123', utc_timestamp),
-(6, 'Débora Mendonça', 'fabio.oliveira.cc@gmail.com', '123', utc_timestamp),
-(7, 'Carlos Lima', 'fabio.oliveira.cc@gmail.com', '123', utc_timestamp);
+(1, 'João da Silva', 'fabio.oliveira.cc+joao@gmail.com', '123', utc_timestamp),
+(2, 'Maria Joaquina', 'fabio.oliveira.cc+maria@gmail.com', '123', utc_timestamp),
+(3, 'José Souza', 'fabio.oliveira.cc+jose@gmail.com', '123', utc_timestamp),
+(4, 'Sebastião Martins', 'fabio.oliveira.cc+sebastiao@gmail.com', '123', utc_timestamp),
+(5, 'Manoel Lima', 'fabio.oliveira.cc+manoel@gmail.com', '123', utc_timestamp),
+(6, 'Débora Mendonça', 'fabio.oliveira.cc+debora@gmail.com', '123', utc_timestamp),
+(7, 'Carlos Lima', 'fabio.oliveira.cc+carlos@gmail.com', '123', utc_timestamp);
 
 insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2);
 
