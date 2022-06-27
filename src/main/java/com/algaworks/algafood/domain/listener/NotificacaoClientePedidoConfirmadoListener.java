82160984931e6e54cdc4a00ce0ca5d8d9e7e7ce4
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-06-27 15:27:29 -0300

    15.13. Reagindo a Domain Events em fases específicas da transação
    15.13. Reagindo a Domain Events em fases específicas da transação

--- src/main/java/com/algaworks/algafood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/com/algaworks/algafood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -3,6 +3,8 @@ package com.algaworks.algafood.domain.listener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.algaworks.algafood.domain.event.PedidoConfirmadoEvent;
 import com.algaworks.algafood.domain.model.Pedido;
@@ -15,7 +17,7 @@ public class NotificacaoClientePedidoConfirmadoListener {
 	@Autowired
 	private EnvioEmailService envioEmail;
 	
-	@EventListener
+	@TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
 	public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
 		
 		Pedido pedido = event.getPedido();
