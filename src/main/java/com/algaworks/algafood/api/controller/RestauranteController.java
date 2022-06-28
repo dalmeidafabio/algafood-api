4ae04a92ea5a9ceb9d2c8d146237ce7944a0c280
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-06-28 16:08:32 -0300

    16.4. Habilitando CORS em controladores e métodos com @CrossOrigin
    16.4. Habilitando CORS em controladores e métodos com @CrossOrigin

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -5,9 +5,8 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +31,7 @@ import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 import com.fasterxml.jackson.annotation.JsonView;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
@@ -68,14 +68,8 @@ public class RestauranteController {
 	
 	@GetMapping
 	@JsonView(RestauranteView.Resumo.class)
-	public ResponseEntity<List<RestauranteModel>> listar(){
-		
-		List<RestauranteModel> restaurantesModel =  restauranteModelAssembler
-				.toCollectionModel(restauranteRepository.findAll());
-		
-		return ResponseEntity.ok()
-				.header(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
-				.body(restaurantesModel);		
+	public List<RestauranteModel> listar(){
+		return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
 	}
 	
 	@GetMapping(params = "projecao=apenas-nome")
