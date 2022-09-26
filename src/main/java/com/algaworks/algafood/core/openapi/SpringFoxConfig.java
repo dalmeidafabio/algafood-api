86a4d7d8b58bbf1e59ba4d5b0ccb8c80107f5790
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-09-26 12:41:36 -0300

    18.12. Descrevendo códigos de status de respostas de forma global
    18.12. Descrevendo códigos de status de respostas de forma global

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -1,15 +1,22 @@
 package com.algaworks.algafood.core.openapi;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -25,6 +32,8 @@ public class SpringFoxConfig {
 	            .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
 	            .paths(PathSelectors.any())
 	            .build()
+            .useDefaultResponseMessages(false)
+            .globalResponses(HttpMethod.GET, globalGetResponseMessages())
     		.apiInfo(apiInfo())
     		.tags(new Tag("Cidades", "Gerencia as cidades."));
   }
@@ -37,5 +46,18 @@ public class SpringFoxConfig {
 			  .contact(new Contact("Algaworkd", "https://www.algaworks.com", "contato@algaworks.com"))
 			  .build();
   }
+  
+  private List<Response> globalGetResponseMessages() {
+	  return Arrays.asList(
+	      new ResponseBuilder()
+	          .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+	          .description("Erro interno do Servidor")
+	          .build(),
+	      new ResponseBuilder()
+	          .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+	          .description("Recurso não possui representação que pode ser aceita pelo consumidor")
+	          .build()
+	  );
+	}  
   
 }
