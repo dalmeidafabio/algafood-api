28399eab7d5091c5d30c7e03ce9f2a5363b1b2db
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-10-17 21:27:50 -0300

    18.21. Corrigindo documentação com substituição de Page
    18.21. Corrigindo documentação com substituição de Page

--- src/main/java/com/algaworks/algafood/api/openapi/controller/GrupoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/GrupoControllerOpenApi.java
@@ -1,84 +1,74 @@
-package com.algaworks.algafood.api.controller.openapi;
+package com.algaworks.algafood.api.openapi.controller;
 
-import java.util.List;
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.algaworks.algafood.api.exceptionhandler.Problem;
-import com.algaworks.algafood.api.model.CidadeModel;
-import com.algaworks.algafood.api.model.input.CidadeInput;
-import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.api.model.GrupoModel;
+import com.algaworks.algafood.api.model.input.GrupoInput;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
-@Api(tags = "Cidades")
-public interface CidadeControllerOpenApi {
-
-	@ApiOperation("Lista as cidades.")
-	public List<Cidade> listar();
-	
+@Api(tags = "Grupos")
+public interface GrupoControllerOpenApi {
 	
 	@ApiResponses({
 		@ApiResponse(responseCode = "404", 
-					description = "Cidade não encontrada", 
+					description = "Grupo não encontrada", 
 					content = @Content(mediaType = "application/json",  
 									schema = @Schema(implementation = Problem.class))),
 		@ApiResponse(responseCode = "400", 
-					description = "ID da cidade é inválido", 
+					description = "ID do Grupo é inválido", 
 					content = @Content(mediaType = "application/json",  
 									schema = @Schema(implementation = Problem.class)))
-	})	
-	@ApiOperation("Busca uma cidade por ID.")
-	public CidadeModel buscar(@ApiParam(name = "ID de uma cidade") Long cidadeId);
+	})		
+	@ApiOperation("Busca um grupo por ID.")
+	public GrupoModel buscar(@PathVariable Long grupoId);
 	
 	@ApiResponses({
 		@ApiResponse(responseCode = "200", 
-					description = "Cidade criada", 
+					description = "Grupo criado", 
 					content = @Content(mediaType = "application/json",  
 									schema = @Schema(implementation = Problem.class))),
 		@ApiResponse(responseCode = "404", 
-					description = "Cidade não encontrada", 
+					description = "Grupo não encontrado", 
 					content = @Content(mediaType = "application/json",  
 									schema = @Schema(implementation = Problem.class)))
 	})
-	@ApiOperation("Cadastra uma cidade.")
-	public CidadeModel adicionar(
-			@ApiParam(name = "corpo", value = "Representação de uma nova cidade.") 
-			CidadeInput cidadeInput);
+	@ApiOperation("Cadastra um Grupo.")	
+	public GrupoModel adicionar(@RequestBody @Valid GrupoInput grupoInput);
 	
 	@ApiResponses({
 		@ApiResponse(responseCode = "200", 
-					description = "Cidade atualizada", 
+					description = "Grupo atualizado", 
 					content = @Content(mediaType = "application/json",  
 									schema = @Schema(implementation = Problem.class))),
 		@ApiResponse(responseCode = "404", 
-					description = "Cidade não encontrada", 
+					description = "Grupo não encontrado", 
 					content = @Content(mediaType = "application/json",  
 									schema = @Schema(implementation = Problem.class)))
-	})		
-	@ApiOperation("Atualiza uma cidade por ID.")
-	public CidadeModel atualizar(
-			@ApiParam(value = "ID de uma cidade.", example = "1") Long cidadeId, 
-			@ApiParam(name = "corpo", value = "Representação de uma cidade com os novos dados.")  
-			CidadeInput cidadeInput);		
+	})
+	@ApiOperation("Cadastra um Grupo.")	
+	public GrupoModel atualizar(@PathVariable Long grupoId, @RequestBody @Valid GrupoInput grupoInput);
 	
 	@ApiResponses({
 		@ApiResponse(responseCode = "204", 
-					description = "Cidade excluída", 
+					description = "Grupo excluído", 
 					content = @Content(mediaType = "application/json",  
 									schema = @Schema(implementation = Problem.class))),
 		@ApiResponse(responseCode = "404", 
-					description = "Cidade não encontrada", 
+					description = "Grupo não encontrada", 
 					content = @Content(mediaType = "application/json",  
 									schema = @Schema(implementation = Problem.class)))
 	})		
-	@ApiOperation("Exclui uma cidade por ID.")
-	public void remover(
-			@ApiParam(value = "ID de uma cidade.", example = "1") 
-			Long cidadeId);
-	
+	@ApiOperation("Exclui um grupo por ID.")	
+	public void remover(@PathVariable Long grupoId);
+
 }
