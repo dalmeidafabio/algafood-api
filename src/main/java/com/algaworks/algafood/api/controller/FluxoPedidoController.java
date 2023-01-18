a1a3596ea866065ed5fb3b6fca0565873b4913b8
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-01-18 16:18:22 -0300

    18.31. Desafio: descrevendo documentação de endpoints de fluxo de pedidos
    18.31. Desafio: descrevendo documentação de endpoints de fluxo de pedidos

--- src/main/java/com/algaworks/algafood/api/controller/FluxoPedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FluxoPedidoController.java
@@ -8,11 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.api.openapi.controller.FluxoPedidoControllerOpenApi;
 import com.algaworks.algafood.domain.service.FluxoPedidoService;
 
 @RestController
 @RequestMapping("/pedidos/{codigoPedido}")
-public class FluxoPedidoController {
+public class FluxoPedidoController implements FluxoPedidoControllerOpenApi  {
 
 	@Autowired
 	private FluxoPedidoService fluxoPedido;
