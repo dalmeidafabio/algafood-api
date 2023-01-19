adc93003661fd766daeb71f3ee79c21c17b6f2f5
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-01-19 15:43:35 -0300

    19.12. Desafio: adicionando hypermedia nos recursos de usuários
    19.12. Desafio: adicionando hypermedia nos recursos de usuários

--- src/main/java/com/algaworks/algafood/api/assembler/UsuarioModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/UsuarioModelAssembler.java
@@ -1,29 +1,44 @@
 package com.algaworks.algafood.api.assembler;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
+import com.algaworks.algafood.api.controller.UsuarioController;
 import com.algaworks.algafood.api.model.UsuarioModel;
 import com.algaworks.algafood.domain.model.Usuario;
 
 @Component
-public class UsuarioModelAssembler {
+public class UsuarioModelAssembler extends RepresentationModelAssemblerSupport<Usuario, UsuarioModel> {
 
 	@Autowired
 	private ModelMapper modelMapper;
 	
+	public UsuarioModelAssembler() {
+		super(UsuarioController.class, UsuarioModel.class);
+	}		
+	
 	public UsuarioModel toModel(Usuario usuario) {
-		return modelMapper.map(usuario, UsuarioModel.class);
+		
+		UsuarioModel usuarioModel = createModelWithId(usuario.getId(), usuario);
+		
+		modelMapper.map(usuario, usuarioModel);
+		
+		usuarioModel.add(linkTo(methodOn(UsuarioController.class)
+				.listar()).withRel("usuarios"));					
+		
+		return usuarioModel;
 	}
 	
-	public List<UsuarioModel> toCollectionModel(Collection<Usuario> usuarios){
-		return usuarios.stream()
-				.map(usuario -> toModel(usuario))
-				.collect(Collectors.toList());
-	}
+	@Override
+	public CollectionModel<UsuarioModel> toCollectionModel(Iterable<? extends Usuario> entities) {
+		return super.toCollectionModel(entities)
+				.add(linkTo(UsuarioController.class).withSelfRel());
+	}	
+
 }
--- src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteUsuarioResponsavelControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteUsuarioResponsavelControllerOpenApi.java
@@ -1,6 +1,6 @@
 package com.algaworks.algafood.api.openapi.controller;
 
-import java.util.List;
+import org.springframework.hateoas.CollectionModel;
 
 import com.algaworks.algafood.api.exceptionhandler.Problem;
 import com.algaworks.algafood.api.model.UsuarioModel;
@@ -21,7 +21,7 @@ public interface RestauranteUsuarioResponsavelControllerOpenApi {
         @ApiResponse(responseCode = "404", description = "Restaurante não encontrado", content = @Content(mediaType = "application/json",  
 				schema = @Schema(implementation = Problem.class)))
     })
-    List<UsuarioModel> listar(
+    CollectionModel<UsuarioModel> listar(
             @ApiParam(value = "ID do restaurante", example = "1", required = true)
             Long restauranteId);
 
