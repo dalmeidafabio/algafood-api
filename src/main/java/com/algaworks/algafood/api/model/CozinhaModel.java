2646b3db18ff8f38f3f5270c1b280546cd83fd20
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-01-20 11:42:34 -0300

    19.15. Adicionando hypermedia em recursos com paginação
    19.15. Adicionando hypermedia em recursos com paginação

--- src/main/java/com/algaworks/algafood/api/model/CozinhaModel.java
+++ src/main/java/com/algaworks/algafood/api/model/CozinhaModel.java
@@ -1,15 +1,21 @@
 package com.algaworks.algafood.api.model;
 
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+
 import com.algaworks.algafood.api.model.view.RestauranteView;
 import com.fasterxml.jackson.annotation.JsonView;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+@Relation(collectionRelation = "cozinhas")
+@ApiModel(value = "Cozinha", description = "Representa uma cozinha")
 @Setter
 @Getter
-public class CozinhaModel {
+public class CozinhaModel extends RepresentationModel<CozinhaModel> {
 
 	@ApiModelProperty(example = "1")
 	@JsonView(RestauranteView.Resumo.class)
--- src/main/java/com/algaworks/algafood/api/model/EstadoModel.java
+++ src/main/java/com/algaworks/algafood/api/model/EstadoModel.java
@@ -1,11 +1,15 @@
 package com.algaworks.algafood.api.model;
 
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+@Relation(collectionRelation = "estados")
+@ApiModel(value = "Estado", description = "Representa um Estado")
 @Setter
 @Getter
 public class EstadoModel extends RepresentationModel<EstadoModel> {
