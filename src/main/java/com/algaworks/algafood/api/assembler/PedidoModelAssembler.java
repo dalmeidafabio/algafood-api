7294d5aa9269525a17b4c7480df2e4a9bc53e7d5
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-01-23 09:53:07 -0300

    19.19. Desafio: adicionando template variables do filtro de pedidos
    19.19. Desafio: adicionando template variables do filtro de pedidos

--- src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
@@ -44,9 +44,16 @@ public class PedidoModelAssembler
         		new TemplateVariable("sort", VariableType.REQUEST_PARAM)
         		);
         
+        TemplateVariables filtroVariables = new TemplateVariables(
+        		new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
+        		new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
+        		new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
+        		new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM));        
+        
         String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
         		
-        pedidoModel.add(Link.of(UriTemplate.of(pedidosUrl, pageVariables), "pedidos"));
+        pedidoModel.add(Link.of(UriTemplate.of(pedidosUrl, 
+        		pageVariables.concat(filtroVariables)), "pedidos"));
         
         //pedidoModel.add(linkTo(PedidoController.class).withRel("pedidos"));
         
