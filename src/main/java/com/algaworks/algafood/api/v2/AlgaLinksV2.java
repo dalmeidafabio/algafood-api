2836108adb789657de183ccc1e941f4ae8b90cce
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-01-25 11:53:11 -0300

    20.15. Desafio: adicionando os recursos de cozinhas na v2 da API
    20.15. Desafio: adicionando os recursos de cozinhas na v2 da API

--- src/main/java/com/algaworks/algafood/api/v2/AlgaLinksV2.java
+++ src/main/java/com/algaworks/algafood/api/v2/AlgaLinksV2.java
@@ -7,6 +7,7 @@ import org.springframework.hateoas.Link;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.api.v2.controller.CidadeControllerV2;
+import com.algaworks.algafood.api.v2.controller.CozinhaControllerV2;
 
 @Component
 public class AlgaLinksV2 {
@@ -18,5 +19,13 @@ public class AlgaLinksV2 {
 	public Link linkToCidades() {
 		return linkToCidades(IanaLinkRelations.SELF.value());
 	}
+	
+	public Link linkToCozinhas(String rel) {
+	    return linkTo(CozinhaControllerV2.class).withRel(rel);
+	}
+
+	public Link linkToCozinhas() {
+	    return linkToCozinhas(IanaLinkRelations.SELF.value());
+	}	
 	
 }
--- src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -4,6 +4,7 @@ import javax.servlet.Filter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -30,6 +31,7 @@ public class WebConfig implements WebMvcConfigurer {
 	
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer.defaultContentType(AlgaMediaTypes.V2_APPLICATION_JSON);
+		//configurer.defaultContentType(AlgaMediaTypes.V2_APPLICATION_JSON);
+		configurer.defaultContentType(MediaType.APPLICATION_JSON);
 	}
 }
