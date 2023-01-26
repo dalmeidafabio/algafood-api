38063c1ac7ccf488bd38cf67e3415d863205b674
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-01-26 16:32:24 -0300

    22.4. Configurando autenticação de usuários em memória
    22.4. Configurando autenticação de usuários em memória

--- src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -1,14 +1,30 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("fabio")
+				.password(passwordEncoder().encode("123456"))
+				.roles("ADMIN")
+			.and()
+				.withUser("joao")
+				.password(passwordEncoder().encode("123456"))
+				.roles("ADMIN");
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {		
@@ -28,6 +44,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 			.and()
 				.csrf().disable();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
