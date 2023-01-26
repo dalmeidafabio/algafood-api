5a54d1463c56487b3e91af89a67efa17d38fc072
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-01-26 09:42:00 -0300

    21.2. Desafio: registrando logs de exceptions não tratadas
    21.2. Desafio: registrando logs de exceptions não tratadas

--- src/main/java/com/algaworks/algafood/api/v1/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/CozinhaController.java
@@ -56,10 +56,6 @@ public class CozinhaController implements CozinhaControllerOpenApi {
 	public PagedModel<CozinhaModel> listar(@PageableDefault(size = 10) Pageable pageable) {
 		log.info("Consultando cozinhas com páginas de {} registros.", pageable.getPageSize());
 		
-		if(true) {
-			throw new RuntimeException("Teste de exception");
-		}
-		
 		Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
 		
 		PagedModel<CozinhaModel> cozinhasPagedModel = pagedResourceAssembler
