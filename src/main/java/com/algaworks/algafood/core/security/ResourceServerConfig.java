8bd3a2fdc901bb1d380d9fc6c98610398967c1af
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-06-10 22:05:31 -0300

    23.12. Configurando a validação de JWT no Resource Server com a chave pública
    23.12. Configurando a validação de JWT no Resource Server com a chave pública

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,14 +1,9 @@
 package com.algaworks.algafood.core.security;
 
-import javax.crypto.spec.SecretKeySpec;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -27,11 +22,4 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 				.jwt();
 	}
 	
-	@Bean
-	public JwtDecoder jwtDecoder() {
-		var secretKey = new SecretKeySpec("ijh5o23jh4o5inuh2345iun354nh3458hn12543198h4329h98237h42"
-				.getBytes(), "HmacSHA256");
-		return NimbusJwtDecoder.withSecretKey(secretKey).build();
-	}
-
 }
