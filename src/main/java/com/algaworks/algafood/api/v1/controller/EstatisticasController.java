3c26e90add973f36eb9ea21eacd5b3ae4e985c1e
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-06-16 14:24:52 -0300

    23.35. Desafio: restringindo acessos aos endpoints de estatísticas
    23.35. Desafio: restringindo acessos aos endpoints de estatísticas

--- src/main/java/com/algaworks/algafood/api/v1/controller/EstatisticasController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/EstatisticasController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.api.v1.AlgaLinks;
 import com.algaworks.algafood.api.v1.openapi.controller.EstatisticasControllerOpenApi;
+import com.algaworks.algafood.core.security.CheckSecurity;
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaReportService;
@@ -32,6 +33,7 @@ public class EstatisticasController implements EstatisticasControllerOpenApi {
 	@Autowired
 	private AlgaLinks algaLinks;
 	
+	@CheckSecurity.Estatisticas.PodeConsultar
 	@GetMapping(path = "/vendas-diarias")
 	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro, 
 			@RequestParam(required = false, defaultValue = "+00:00") String timeOffSet){
@@ -40,6 +42,7 @@ public class EstatisticasController implements EstatisticasControllerOpenApi {
 		
 	}
 	
+	@CheckSecurity.Estatisticas.PodeConsultar
 	@GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_PDF_VALUE)
 	public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro, 
 			@RequestParam(required = false, defaultValue = "+00:00") String timeOffSet){
@@ -56,6 +59,7 @@ public class EstatisticasController implements EstatisticasControllerOpenApi {
 		
 	}
 	
+	@CheckSecurity.Estatisticas.PodeConsultar
 	@Override
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public EstatisticasModel estatisticas() {
--- src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
+++ src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
@@ -143,5 +143,14 @@ public @interface CheckSecurity {
 		public @interface PodeConsultar {}
 		
 	}		
+	
+	public interface Estatisticas {
+		
+		@PreAuthorize("hasAuthority('SCOPE_READ') and hasAuthority('GERAR_RELATORIOS')")
+		@Retention(RUNTIME)
+		@Target(METHOD)		
+		public @interface PodeConsultar {}
+		
+	}	
 
 }
