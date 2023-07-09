ed129b1da98552b587921f838ddf80a345ae0957
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-07-09 12:58:51 -0300

    23.44. Customizando a página de OAuth2 Approval
    23.44. Customizando a página de OAuth2 Approval

--- src/main/java/com/algaworks/algafood/core/security/authorizationserver/SecurityController.java
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/SecurityController.java
@@ -1,30 +1,19 @@
 package com.algaworks.algafood.core.security.authorizationserver;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.provider.ClientDetails;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SecurityController {
-	
-    @Autowired
-    private ClientDetailsService clientDetailsService;	
 
 	@GetMapping("/login")
 	public String login() {
 		return "pages/login";
 	}
 	
-    @GetMapping("/oauth/confirm_access")
-    public ModelAndView approval(@RequestParam("client_id") String clientId){
-        ClientDetails client = clientDetailsService.loadClientByClientId(clientId);
-        ModelAndView modelAndView = new ModelAndView("pages/approval");
-        modelAndView.addObject("scopes", client.getScope());
-        return modelAndView;
-    }
+	@GetMapping("/oauth/confirm_access")
+	public String approval() {
+		return "pages/approval";
+	}
 	
 }
