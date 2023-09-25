0c27722139516ec53743c07c72432d4844dda0f9
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-09-25 15:45:06 -0300

    26.23. Corrigindo documentação no Swagger UI para upload de arquivos
    26.23. Corrigindo documentação no Swagger UI para upload de arquivos

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
@@ -4,14 +4,15 @@ import java.io.IOException;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.algaworks.algafood.api.v1.model.FotoProdutoModel;
 import com.algaworks.algafood.api.v1.model.input.FotoProdutoInput;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -20,15 +21,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Restaurantes - Produtos - Fotos dos Produtos", description = "Gerencia as Fotos dos Produtos")
 public interface RestauranteProdutoFotoControllerOpenApi {
 
-    FotoProdutoModel atualizarFoto(
-            Long restauranteId,
-            Long produtoId,
-            FotoProdutoInput fotoProdutoInput,
-            MultipartFile arquivo) throws IOException;
+	@Operation(summary = "Atualiza a foto do produto de um restaurante")
+	FotoProdutoModel atualizarFoto(@Parameter(description = "Id do restaurante", example = "1", required = true) Long restauranteId,
+								   @Parameter(description = "Id do produto", example = "2", required = true) Long produtoId,
+								   @RequestBody(required = true) FotoProdutoInput fotoProdutoInput) throws IOException;
 
-    void excluir(
-            Long restauranteId,
-            Long produtoId);
+	ResponseEntity<Void> excluir(Long restauranteId,Long produtoId);
 
 	@Operation(summary = "Busca a foto do produto de um restaurante", responses = {
 			@ApiResponse(responseCode = "200", content = {
@@ -42,4 +40,5 @@ public interface RestauranteProdutoFotoControllerOpenApi {
 	@Operation(hidden = true)
 	ResponseEntity<?> servir(Long restauranteId, Long produtoId, String acceptHeader)
 			throws HttpMediaTypeNotAcceptableException;
+
 }
