fcfcabaa2d2f03f665db948cda82ee487d042dcd
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-09-25 10:22:02 -0300

    26.14. Descrevendo o modelo de representação de problema
    26.14. Descrevendo o modelo de representação de problema

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -23,7 +23,7 @@ public interface CidadeControllerOpenApi {
 	@Operation(summary = "Busca uma cidade por Id", responses = {
 			@ApiResponse(responseCode = "200"),
 			@ApiResponse(responseCode = "400", description = "ID da cidade inválido",
-					content = @Content(schema = @Schema)
+					content = @Content(schema = @Schema(ref = "Problema"))
 			)
 	})
 	CidadeModel buscar(@Parameter(description = "ID de uma cidade", example = "1", required = true) Long cidadeId);
--- src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -1,18 +1,26 @@
 package com.algaworks.algafood.core.springdoc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+
+import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.OAuthFlow;
 import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
 
@@ -34,7 +42,7 @@ public class SpringDocConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("AlgaFood API")
-                        .version("v1")
+                        .version("v1")	
                         .description("REST API do AlgaFood")
                         .license(new License()
                                 .name("Apache 2.0")
@@ -43,7 +51,9 @@ public class SpringDocConfig {
                 ).externalDocs(new ExternalDocumentation()
                         .description("AlgaWorks")
                         .url("https://algaworks.com")
-                );
+                ).components(new Components().schemas(
+                        gerarSchemas()
+                ));                
     }
 
     @Bean
@@ -81,7 +91,19 @@ public class SpringDocConfig {
                             })
                     );
         };
-    }  
+    }
+    
+    private Map<String, Schema> gerarSchemas() {
+        final Map<String, Schema> schemaMap = new HashMap<>();
+
+        Map<String, Schema> problemSchema = ModelConverters.getInstance().read(Problem.class);
+        Map<String, Schema> problemObjectSchema = ModelConverters.getInstance().read(Problem.Object.class);
+
+        schemaMap.putAll(problemSchema);
+        schemaMap.putAll(problemObjectSchema);
+
+        return schemaMap;
+    }
 
 //    @Bean
 //    public GroupedOpenApi groupedOpenApi() {
