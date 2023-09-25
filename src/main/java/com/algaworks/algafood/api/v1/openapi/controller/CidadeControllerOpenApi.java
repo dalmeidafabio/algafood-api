2b83f320009614bcb8b5e78daa580fa9cafe3651
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-09-25 10:06:03 -0300

    26.12. Descrevendo códigos de status de respostas em endpoints específicos
    26.12. Descrevendo códigos de status de respostas em endpoints específicos

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -4,7 +4,10 @@ import com.algaworks.algafood.api.v1.model.CidadeModel;
 import com.algaworks.algafood.api.v1.model.input.CidadeInput;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
@@ -17,14 +20,19 @@ public interface CidadeControllerOpenApi {
 	@Operation(summary = "Lista as cidades")
 	CollectionModel<CidadeModel> listar();
 
-	@Operation(summary = "Busca uma cidade por Id")
+	@Operation(summary = "Busca uma cidade por Id", responses = {
+			@ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "400", description = "ID da cidade inválido",
+					content = @Content(schema = @Schema)
+			)
+	})
 	CidadeModel buscar(@Parameter(description = "ID de uma cidade", example = "1", required = true) Long cidadeId);
 
 	@Operation(summary = "Cadastra uma cidade", description = "Cadastro de uma cidade, " +
 			"necessita de um estado e um nome válido")
 	CidadeModel adicionar(@RequestBody(description = "Representação de uma nova cidade", required = true) CidadeInput cidadeInput);
 
-	@Operation(summary = "Atualiza uma cidade por ID")
+	@Operation(summary = "Atualizado uma cidade por ID")
 	CidadeModel atualizar(@Parameter(description = "ID de uma cidade", example = "1", required = true) Long cidadeId,
 						  @RequestBody(description = "Representação de uma cidade com dados atualizados", required = true) CidadeInput cidadeInput);
 
