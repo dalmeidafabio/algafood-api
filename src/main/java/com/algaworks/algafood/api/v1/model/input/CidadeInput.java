eb08b30dedfa41b2896068f30862f7eff6c4f03b
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-09-25 09:46:32 -0300

    26.9. Descrevendo modelos de representações e suas propriedades
    26.9. Descrevendo modelos de representações e suas propriedades

--- src/main/java/com/algaworks/algafood/api/v1/model/input/CidadeInput.java
+++ src/main/java/com/algaworks/algafood/api/v1/model/input/CidadeInput.java
@@ -1,20 +1,23 @@
 package com.algaworks.algafood.api.v1.model.input;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
 @Setter
+@Getter
 public class CidadeInput {
 
+	@Schema(example = "Uberlândia")
 	@NotBlank
 	private String nome;
-	
+
 	@Valid
 	@NotNull
 	private EstadoIdInput estado;
+	
 }
--- src/main/java/com/algaworks/algafood/api/v1/model/input/EstadoInput.java
+++ src/main/java/com/algaworks/algafood/api/v1/model/input/EstadoInput.java
@@ -1,15 +1,17 @@
 package com.algaworks.algafood.api.v1.model.input;
 
-import javax.validation.constraints.NotBlank;
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
 
 @Setter
 @Getter
 public class EstadoInput {
 
+	@Schema(example = "Minas Gerais")
 	@NotBlank
-	private String nome; 
+	private String nome;
 	
 }
