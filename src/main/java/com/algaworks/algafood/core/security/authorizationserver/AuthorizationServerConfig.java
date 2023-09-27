f865c78294b96d13024b1098f388eaa4eb861a27
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-09-27 12:09:24 -0300

    27.12. Testando o fluxo Authorization Code + PKCE + S256 e corrigindo problemas
    27.12. Testando o fluxo Authorization Code + PKCE + S256 e corrigindo problemas

--- src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -81,15 +81,15 @@ public class AuthorizationServerConfig {
 						.accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
 						.accessTokenTimeToLive(Duration.ofMinutes(15))
 						.build())
-				.redirectUri("http://localhost:8080/authorized")
-				.redirectUri("http://localhost:8080/swagger-ui/oauth2-redirect.html")
+				.redirectUri("http://127.0.0.1:8080/authorized")
+				.redirectUri("http://127.0.0.1:8080/swagger-ui/oauth2-redirect.html")
 				.clientSettings(ClientSettings.builder()
 						.requireAuthorizationConsent(true)
 						.build())
 				.build();
 		
 		RegisteredClient foodanalytics = RegisteredClient
-				.withId("2")
+				.withId("3")
 				.clientId("analytics-web")
 				.clientSecret(passwordEncoder.encode("web123"))
 				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
