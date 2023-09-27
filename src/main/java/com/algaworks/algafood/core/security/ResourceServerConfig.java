772739cbd14f502c3c6957294a3a40c4e2726716
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-09-27 10:55:03 -0300

    27.11. Implementando um cliente com o fluxo Authorization Code
    27.11. Implementando um cliente com o fluxo Authorization Code

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,7 +25,7 @@ public class ResourceServerConfig {
 				//.oauth2ResourceServer().opaqueToken();
 				.oauth2ResourceServer().jwt();
 		
-		return http.build();
+		return http.formLogin(Customizer.withDefaults()).build();
 	}
 	
 }
