45f7a37d4764d5fb1fded799440c80389e161418
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-09-28 15:23:07 -0300

    28.6. Atualizando o Spring Authorization Server
    28.6. Atualizando o Spring Authorization Server

--- src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -4,15 +4,14 @@ import java.io.InputStream;
 import java.security.KeyStore;
 import java.util.HashSet;
 import java.util.Set;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -23,12 +22,13 @@ import org.springframework.security.oauth2.server.authorization.OAuth2Authorizat
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.RequestMatcher;
-
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.repository.UsuarioRepository;
 import com.nimbusds.jose.jwk.JWKSet;
@@ -43,9 +43,8 @@ public class AuthorizationServerConfig {
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain authFilterChain(HttpSecurity http) throws Exception {
-//        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-        OAuth2AuthorizationServerConfigurer<HttpSecurity> authorizationServerConfigurer =
-                new OAuth2AuthorizationServerConfigurer<>();
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
+                new OAuth2AuthorizationServerConfigurer();
 
         authorizationServerConfigurer.authorizationEndpoint(
                 customizer -> customizer.consentPage("/oauth2/consent"));
@@ -53,19 +52,23 @@ public class AuthorizationServerConfig {
         RequestMatcher endpointsMatcher = authorizationServerConfigurer
                 .getEndpointsMatcher();
 
-        http.requestMatcher(endpointsMatcher)
-            .authorizeRequests(authorizeRequests ->
-                    authorizeRequests.anyRequest().authenticated()
-            )
+        http.securityMatcher(endpointsMatcher)
+            .authorizeHttpRequests(authorize -> {
+                authorize.anyRequest().authenticated();
+            })
             .csrf(csrf -> csrf.ignoringRequestMatchers(endpointsMatcher))
+            .formLogin(Customizer.withDefaults())
+            .exceptionHandling(exceptions -> {
+                exceptions.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"));
+            })
             .apply(authorizationServerConfigurer);
 
         return http.formLogin(customizer -> customizer.loginPage("/login")).build();
     }
 
     @Bean
-    public ProviderSettings providerSettings(AlgaFoodSecurityProperties properties) {
-        return ProviderSettings.builder()
+    public AuthorizationServerSettings providerSettings(AlgaFoodSecurityProperties properties) {
+        return AuthorizationServerSettings.builder()
                 .issuer(properties.getProviderUrl())
                 .build();
     }
